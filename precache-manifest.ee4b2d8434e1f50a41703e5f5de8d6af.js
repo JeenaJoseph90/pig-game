42ea6ca4ff5e277c2b002d9d954ef5566d73b947
JeenaJoseph90 <45370725+JeenaJoseph90@users.noreply.github.com>
2020-02-07 12:05:28 +0400

    Updates

---
+++ precache-manifest.ee4b2d8434e1f50a41703e5f5de8d6af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "348a931438a3f166ebdc1b6a20a61ac3",
+    "url": "/pig-game/index.html"
+  },
+  {
+    "revision": "d317592b52747e0c3af3",
+    "url": "/pig-game/static/css/main.0ca1faf9.chunk.css"
+  },
+  {
+    "revision": "9ece7b313f34f36b6daa",
+    "url": "/pig-game/static/js/2.6354d975.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pig-game/static/js/2.6354d975.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d317592b52747e0c3af3",
+    "url": "/pig-game/static/js/main.b22cc418.chunk.js"
+  },
+  {
+    "revision": "71829bc5c4a2f4d8232d",
+    "url": "/pig-game/static/js/runtime-main.414a0911.js"
+  },
+  {
+    "revision": "f8af2c81d948bd31a95facd389f6de61",
+    "url": "/pig-game/static/media/flag.f8af2c81.gif"
+  }
+]);
